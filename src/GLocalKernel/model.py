b1bfd6c2488d7a46fc63d8c6a5691daa3165676c
Peyman Alavi <alavi.peyman.99@gmail.com>
2025-02-24 18:51:45 +0100

    feat: implement complete architecture of the GLocalKernel model

---
+++ src/GLocalKernel/model.py
@@ -0,0 +1,42 @@
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+
+
+class GlobalLocalKernelModel(nn.Module):
+    def __init__(
+        self,
+        local_kernel_net: nn.Module,
+        gk_size: int,
+        dot_scale: float,
+        n_m: int,
+    ) -> None:
+        super(GlobalLocalKernelModel, self).__init__()
+
+        self.local_kernel_net = local_kernel_net
+        self.gk_size = gk_size
+        self.dot_scale = dot_scale
+
+        self.conv_kernel = nn.Parameter(torch.Tensor(n_m, self.gk_size**2))
+        nn.init.normal_(self.conv_kernel, mean=0.0, std=0.1)
+
+    def forward(
+        self, x: torch.Tensor, x_local: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        gk = self.global_kernel(x_local)
+        x = self.global_conv(x, gk)
+        x, reg_loss = self.local_kernel_net(x)
+        return x, reg_loss
+
+    def global_kernel(self, x: torch.Tensor) -> torch.Tensor:
+        avg_pooling = torch.mean(x, dim=1).view(1, -1)
+        gk = torch.matmul(avg_pooling, self.conv_kernel) * self.dot_scale
+        gk = gk.view(1, 1, self.gk_size, self.gk_size)
+        return gk
+
+    def global_conv(self, x: torch.Tensor, W: torch.Tensor) -> torch.Tensor:
+        x = x.unsqueeze(0).unsqueeze(0)
+        conv2d = nn.ReLU()(nn.functional.conv2d(x, W, stride=1, padding=1))
+        conv2d = conv2d.squeeze(0).squeeze(0)
+        return conv2d
